199d08cb770f6009457d7860c34e1010e10cc9ae
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-17 16:11:11 -0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,15 @@
 
 /**
  *main - print a message with printf
-  *
-   *Return: end program
-    */
-    int main(void)
-    {
-	printf("%lu\n", sizeof(char));
-			printf("%lu\n", sizeof(int));
-					printf("%lu\n", sizeof(long int));
-							printf("%lu\n", sizeof(long long int));
-									printf("%lu\n", sizeof(float));
-											return (0);
-											}
+ *
+ *Return: end program
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
