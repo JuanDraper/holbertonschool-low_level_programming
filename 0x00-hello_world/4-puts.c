b3505d67df3ec4d62e9188d519477f56d1129c4e
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-17 15:35:56 -0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
-puts ("Programming is like building a multilingual puzzle" "\n")
-
+#include<stdio.h>
+int main()
+{
+      char String[] = "Programming is like building a multilingual puzzle";
+         puts(Mystr); 
+         return 0;
+}
