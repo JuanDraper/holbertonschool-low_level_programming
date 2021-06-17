b5a1b51d7c2e0e9659d20de5e9cac367772267c3
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-17 15:53:04 -0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/* 
-*main: this will make the input of the String
-*/
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{ puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
