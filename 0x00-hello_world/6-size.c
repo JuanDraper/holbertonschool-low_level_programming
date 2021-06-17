510037138d344c9a7ceaf8f42edbd7fa6da31299
JuanDraper <e146613@gmail.com>
2021-06-17 12:09:41 -0700

    fixing

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - print a message with printf
+  *
+   *Return: end program
+    */
+    int main(void)
+    {
+	printf("%lu\n", sizeof(char));
+			printf("%lu\n", sizeof(int));
+					printf("%lu\n", sizeof(long int));
+							printf("%lu\n", sizeof(long long int));
+									printf("%lu\n", sizeof(float));
+											return (0);
+											}
