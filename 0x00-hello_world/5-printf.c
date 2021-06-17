cd84f28808a81ecd57bb9adb8a136b371bb5c27f
JuanDraper <e146613@gmail.com>
2021-06-17 12:01:35 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - print the msj with printf
+ *
+ *Return: finish
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
