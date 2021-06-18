744f6d8f5912936488e4a2acdeadf9bedef1cdd3
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-18 18:40:07 -0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- *main - Entry point
-*
-*Return: Always 0 (success)
-*Description: This program prints all letters
-*Return: Returns 0 
-*/
+ *main - who knows the abc?
+ *
+ *Return: end program
+ */
 int main(void)
-char c;
-for (c="a" ; c <= "z" ; c++)
 {
-putchar (c);
-putchar (\n);
-}
-return (0);
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
 }
