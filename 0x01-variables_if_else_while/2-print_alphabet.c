adc5f102c393dedfe90adafdfb1f133efb157075
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-18 18:42:54 -0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 /**
- *main - who knows the abc?
- *
- *Return: end program
+*main - who knows the abc?
+*
+*Return: end program
  */
 int main(void)
 {
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-	putchar('\n');
-	return (0);
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
 }
