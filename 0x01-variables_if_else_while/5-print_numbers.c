0c313df757d982cf171c0c9d9522fbf81791ac08
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-18 19:01:30 -0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,8 +2,12 @@
 /**
  *main - the abc excep 2 letters
  *
- *Return: end program
- */
+*Return: end program
+*/
 int main(void)
-  int n
-  for (n = '0' ;
+{
+int n
+for (n = '0' ; n <= 9 ; n++)
+printf ("%d" , n);
+return (0)
+}
