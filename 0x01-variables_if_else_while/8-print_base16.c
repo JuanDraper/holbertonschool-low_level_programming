16029aaf97f85dde1b2eae06f9333822c729be54
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-19 01:07:42 -0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 /**
- *main - print  numbers
- *
- *Return: end program
- */
+*main - print  numbers
+*
+*Return: end program
+*/
 int main(void)
 {
+char hexa;
+for (hexa = '0' ; hexa <= '9' ; hexa++)
+putchar(i);
+for ( hexa = 'a' ; hexa <= 'f' ; hexa++)
+putchar(i);
+putchar('\n');
+return (0);
+}
