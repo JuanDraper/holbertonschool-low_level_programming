315407de657101496d7034690a13dd4d8f22d975
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-19 01:11:06 -0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,7 +9,7 @@ int main(void)
 char hexa;
 for (hexa = '0' ; hexa <= '9' ; hexa++)
 putchar(hexa);
-for ( hexa = 'a' ; hexa <= 'f' ; hexa++)
+for (hexa = 'a' ; hexa <= 'f' ; hexa++)
 putchar(hexa);
 putchar('\n');
 return (0);
