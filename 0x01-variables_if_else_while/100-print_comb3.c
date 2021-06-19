8476d8edbab393741ea17696310c0155f2cf7232
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-19 01:38:52 -0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,22 +8,22 @@ int main(void)
 {
 int i;
 int a;
-  for (i = 0; i <= 9; i++)
-    {
-      for (a = i; a <= 9; a++)
-	{
-	  if (i != a)
-	    {
-	      putchar(i + '0');
-	      putchar(a + '0');
-	      if (i < 8)
-		{
-		  putchar(',');
-		  putchar(' ');
-		}
-	    }
-	}
-    }
-  putchar('\n');
-  return (0);
+for (i = 0; i <= 9; i++)
+{
+for (a = i; a <= 9; a++)
+{
+if (i != a)
+{
+putchar(i + '0');
+putchar(a + '0');
+if (i < 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
