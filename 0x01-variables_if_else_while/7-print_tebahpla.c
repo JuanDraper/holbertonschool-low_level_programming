5dde8bf87aec9674bc38042a2f7f9f35e6e9561f
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-19 00:55:22 -0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 /**
- *main - print  numbers
- *
- *Return: end program
- */
+*main - print  numbers
+*
+*Return: end program
+*/
+int main (void)
+{
+char c;
+for (c = 'z' ; z >= 'a' ; c--)
+putchar (c);
+putchar ('\n');
+return(0);
+}
