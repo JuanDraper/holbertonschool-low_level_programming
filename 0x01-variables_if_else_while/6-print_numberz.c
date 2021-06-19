c29f662e58f3e2205a4304b1e30902890a527e39
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-19 00:43:52 -0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,17 @@
+  
 #include <stdio.h>
-/**  
 
-*main - print numbers
-*
-*Return: end program
-*/
+/**
+ *main - print numbers
+ *
+ *Return: end program
+ */
 int main(void)
 {
-int num;
-for (num = 0 ; num <= 9 ; num++)
-{
-putchar(num + '0');
-}
-putchar('\n');
-return(0);
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		putchar(num + '0');
+	putchar('\n');
+	return (0);
 }
