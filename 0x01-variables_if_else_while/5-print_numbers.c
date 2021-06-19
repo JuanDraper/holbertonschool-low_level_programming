43dec2e11c3167c3bff81727a56cd6480a510da2
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-19 00:07:22 -0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
-*main:will print numbers
+*main - will print numbers
 *
 *Return: 0
 */
 int main(void)
 {
 char n;
-for(n = '0'; n <= '9'; n++)
+for (n = '0'; n <= '9'; n++)
 {
 putchar(n);
 }
