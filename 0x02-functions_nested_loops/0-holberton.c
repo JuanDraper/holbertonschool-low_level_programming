1a1b1c944712ae416ee737463dab73943ac230c7
JuanDraper <e146613@gmail.com>
2021-06-22 15:21:56 -0700

    0-holberton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ *  *main - print a message with printf
+ *   *
+ *    *Return: end program
+ *     */
+int main (void)
+{
+	char h[] = 'Holberton';
+	int i;
+	for ( i = 0 ; i < 9 ; i++ )
+		_putchar( h[i]);
+	
+	return (0);
+}#include "holberton.h"
+
+/**
+ *  *main - print a message with printf
+ *   *
+ *    *Return: end program
+ *     */
+int main (void)
+{
+	char h[] = 'Holberton';
+	int i;
+	for ( i = 0 ; i < 9 ; i++ )
+		_putchar( h[i]);
+	
+	return (0);
+}
