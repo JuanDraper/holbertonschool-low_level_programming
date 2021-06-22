1eb855b313987c76a2694e937e9226d80c3b76ea
JuanDraper <e146613@gmail.com>
2021-06-22 16:00:51 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+nclude "holberton.h"
+
+/**
+ * *  *main - print a message
+ * *
+ * *    *Return: end program
+ * *
+ * *
+ * /
+    void print_alphabet(void)
+    {
+    char c ;
+    for ( c = 'a' ; c <= 'z' ; c++)
+    _putchar (c);
+  _putchar ('\n');
+  return (0);
+  }
