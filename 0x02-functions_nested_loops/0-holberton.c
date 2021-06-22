6862b8043732c2243be100c15219d7b403003156
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-22 19:35:14 -0300

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,21 +1,21 @@
 nclude "holberton.h"
 
 /**
- *  *main - print a message with printf
- *   *
- *    *Return: end program
- *     */
+*  *main - print a message with printf
+*   *
+*    *Return: end program
+*     */
 int main(void)
 {
-		_putchar('H');
-			_putchar('o');
-				_putchar('l');
-					_putchar('b');
-						_putchar('e');
-							_putchar('r');
-								_putchar('t');
-									_putchar('o');
-										_putchar('n');
-											_putchar('\n');
-												return (0);
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+return (0);
 }
