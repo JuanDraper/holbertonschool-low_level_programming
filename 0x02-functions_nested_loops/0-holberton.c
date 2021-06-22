7f31073efc2cb1c1dea0266b47b9c908bb56f8f2
JuanDraper <e146613@gmail.com>
2021-06-22 14:51:12 -0700

    commitin

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *  *main - print numbers
+ *   *
+ *    *Return: end program
+ *     */
+int main (void)
+{
+	char H[] = "Holberton" ;
+	int i ;
+	
+	for ( i = 0 ; i < 9 ; i++ )
+		_putchar ( H[i]);
+	return (0);
+}
+
