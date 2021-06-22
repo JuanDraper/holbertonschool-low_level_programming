581331ed213ceb46abaefac74fda75d064ef4deb
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-22 20:03:12 -0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
 nclude "holberton.h"
 
 /**
- * *  *main - print a message
- * *
- * *    *Return: end program
- * *
- * *
- * /
-    void print_alphabet(void)
-    {
-    char c ;
-    for ( c = 'a' ; c <= 'z' ; c++)
-    _putchar (c);
-  _putchar ('\n');
-  return (0);
-  }
+*main - print a message
+*
+*Return: end program
+*/
+void print_alphabet(void)
+{
+char c ;
+for ( c = 'a' ; c <= 'z' ; c++)
+_putchar (c);
+_putchar ('\n');
+return (0);
+}
