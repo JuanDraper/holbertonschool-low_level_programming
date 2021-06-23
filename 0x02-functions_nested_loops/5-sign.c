d238bf33a19a831033d1d798cd8c7b390631fc63
JuanDraper <e146613@gmail.com>
2021-06-22 17:05:28 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+nclude "holberton.h"
+/**
+ * * int print_sign - return 1 all about numbers and signs
+ * * 
+ * * Return : 1 , -1 , 0 
+ * */
+int print_sign(int n)
+		if ( n < 0)
+{ _putchar ('-');
+	return (-1);
+}
+else if ( n == 0)
+{
+	_putchar('0');
+	return (0);
+
+}
+else 
+{
+	_putchar ('+')
+}
