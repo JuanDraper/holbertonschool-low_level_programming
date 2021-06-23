a64d852ca55049ebc32776eb9886ab1d9539f1f6
JuanDraper <e146613@gmail.com>
2021-06-22 17:54:51 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+nclude "holberton.h"
+/**
+ * * print_last_digit - tells the absolute value of an integer
+ * * @i: int type number
+ * * Return: absolute value of @n
+ * */
+int print_last_digit(int i)
+{
+	int digit = i % 10;
+	if (i > 0)
+	{
+	_putchar(digit + '0');
+	return (digit);
+	}
+	else if (i == 0)
+	{
+	_putchar( n + '0');
+	}
+	else 
+	{
+		_putchar(last *(-1) + '0');
+		return (last * (-1));
+	}
+}
+	
