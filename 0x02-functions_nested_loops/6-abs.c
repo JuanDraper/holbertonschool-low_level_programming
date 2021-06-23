573732b24d6c187608a00aff18a7042fa0b3b040
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-22 21:42:23 -0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
 * _abs - tells the absolute value of an integer
-* @n: int type number
+* @i: int type number
 * Return: absolute value of @n
 */
 int _abs(int i)
 {
-if (i <0)
+if (i < 0)
 { return (i * -1);
 }
 else
