5e011ee7ee579a69213df9c90dc8aa6c2ec5399d
JuanDraper <e146613@gmail.com>
2021-06-22 17:38:41 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+nclude "holberton.h"
+/**
+ *  * _abs - tells the absolute value of an integer
+ *   * @n: int type number
+ *    * Return: absolute value of @n
+ *     */
+int _abs(int i)
+{
+	if (i <0)
+	{ return (i * -1);
+	}
+	else
+	{
+		return (i);
+	}
