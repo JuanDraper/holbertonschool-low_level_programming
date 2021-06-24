ae68fbb5895e4887ee6ff2b83d593e25874d430c
JuanDraper <e146613@gmail.com>
2021-06-24 11:15:29 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* print_most_numbers - Entry point
+*
+*Return: if is lower or upper return 1 else 0
+*/
+void print_most_numbers(void)
+{
+int i;
+if (i = 0; i < 10; i++)
+{
+if ( i != 2 && i != 4)
+{
+_putchar("%d", i);
+}
+}
+_putchar ('\n');
+}
