0d6143db111c12ae98e2fb303bf6367236c780db
JuanDraper <e146613@gmail.com>
2021-06-24 10:42:01 -0700

    fix

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 
 /**
-*_isupper - Entry point
+*_isdigit - Entry point
 *@c: charater to check
 *Return: if is lower or upper return 1 else 0
 */
 int _isdigit(int c)
 {
-if ((c >= 'a' && c <='z')  || (c >= 'A' && c <= 'Z'))
-return (0);
-else
+if (c >= 48 && c <=57)
 return (1);
+else
+return (0);
 }
