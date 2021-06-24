4437e3d18d292c331303ad27d68166e1fed0dfc3
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-24 14:23:28 -0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 /**
 * _isupper - return 1 if c is lowercase and returns 0 otherwise
 * @c: takes in a character
-* Return: 0 for uppercase, 1 for lowercase
+* Return: 1 for uppercase, 0 for lowercase
  */
 int _isupper(int c)
 {
