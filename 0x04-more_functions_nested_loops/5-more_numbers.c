6bac307d7ddf1bf7c38f7814c8a94622dec9f42f
JuanDraper <e146613@gmail.com>
2021-06-24 11:41:15 -0700

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,10 +14,9 @@ for (i = 0 ; i <= 9 ; i++)
 int j;       
 for (j = 0 ; j <= 14 ; j++)
 {
-_putchar( j + '0');
-}
-_putchar('\n');
-}
+if (c > 9)
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0
 _putchar('\n');
 }
 
