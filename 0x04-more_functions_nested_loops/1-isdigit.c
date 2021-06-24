c6b8589bb6d86dfc2ea2de0f3d7d8010b8ca8fd0
JuanDraper <e146613@gmail.com>
2021-06-24 10:36:08 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "holberton"
+
+/**
+*_isupper - Entry point
+*@c: charater to check
+*Return: if is lower or upper return 1 else 0
+*/
+int _isdigit(int c)
+{
+if ((c >= 'a' && c <='z')  || (c >= 'A' && c <= 'Z'))
+return (0);
+else
+return (1);
+}
