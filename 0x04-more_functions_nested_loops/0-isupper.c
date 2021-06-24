11b99dad96b3ad8b0ba0adcc7bf5ae0a3e455183
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-24 14:20:11 -0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-* _islower - return 1 if c is lowercase and returns 0 otherwise
+* _isupper - return 1 if c is lowercase and returns 0 otherwise
 * @c: takes in a character
 * Return: 0 for uppercase, 1 for lowercase
  */
