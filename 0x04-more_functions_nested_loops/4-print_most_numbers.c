f84ad65b3b510286b00b56c46581da233d517f75
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-24 15:22:21 -0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,12 @@
 */
 void print_most_numbers(void)
 {
-int i;
-if (i = 0; i < 10; i++)
+char i;
+if (i = '0'; i < '10'; i++)
 {
-if ( i != 2 && i != 4)
+if ( i != '2' && i != '4')
 {
-_putchar(i + '0');
+_putchar(i);
 }
 }
 _putchar ('\n');
