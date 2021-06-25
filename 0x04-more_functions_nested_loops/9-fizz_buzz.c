73d61719dac298fd0f7d64dc572c8bcb0b55bd9a
JuanDraper <e146613@gmail.com>
2021-06-24 17:44:59 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,39 +1,37 @@
 #include "holberton.h"
 #include <stdio.h>
 /*
- * main - Fizz Buzz!
- *
- *Rwturn: nothing
- *
- *
- */
+* main - Fizz Buzz!
+*
+*Rwturn: nothing
+*
+*
+*/
 int main(void)
 {
 int i;
 for ( i = 0 ; i < 100 ; i++)
 {
-	if( i == 0)
-	{
-		printf("%d ", i);
-	}
-	else if((i % 3 == 0) && (i % 5 != 0))
-       	{
-		printf("Fizz ");
-	}
-	else if((i % 5 == 0) && ( i % 3 != 0))
-	{
-		printf("Buzz ");
-	}
-	else if(( i % 3 == 0) && (i % 5 == 0))
-	{
-		printf("Fizz Buzz ");
-	}
-	else
-
-		printf("%d " , i);
-
+if( i == 0)
+{
+printf("%d ", i);
 }
-	return (0);
-	}
+else if((i % 3 == 0) && (i % 5 != 0))
+{
+printf("Fizz ");
+}
+else if((i % 5 == 0) && ( i % 3 != 0))
+{
+printf("Buzz ");
+}
+else if(( i % 3 == 0) && (i % 5 == 0))
+{
+printf("Fizz Buzz ");
+}
+else
+printf("%d " , i);
+}
+return (0);
+}
 
 
