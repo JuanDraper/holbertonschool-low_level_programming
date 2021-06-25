01a091de189ea58214603e29da0450c85b97c744
JuanDraper <e146613@gmail.com>
2021-06-24 17:40:52 -0700

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+/*
+ * main - Fizz Buzz!
+ *
+ *Rwturn: nothing
+ *
+ *
+ */
+int main(void)
+{
+int i;
+for ( i = 0 ; i < 100 ; i++)
+{
+	if( i == 0)
+	{
+		printf("%d ", i);
+	}
+	else if((i % 3 == 0) && (i % 5 != 0))
+       	{
+		printf("Fizz ");
+	}
+	else if((i % 5 == 0) && ( i % 3 != 0))
+	{
+		printf("Buzz ");
+	}
+	else if(( i % 3 == 0) && (i % 5 == 0))
+	{
+		printf("Fizz Buzz ");
+	}
+	else
+
+		printf("%d " , i);
+
+}
+	return (0);
+	}
+
+
