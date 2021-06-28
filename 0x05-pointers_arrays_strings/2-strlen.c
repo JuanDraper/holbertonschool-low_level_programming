f69f598f0814464eff6a1f8bc75812a1b2df893b
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-28 20:17:57 -0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
-/*
- * _strlen - tells the length of a string
- *@i: the position refference
- * Return: end function
+
+/**
+ * _strlen - return the length of the string
+ * @s: char type pointer
+ * Return: length of string
  */
+
 int _strlen(char *s)
 {
 int i = 0;
-while ( s[i] != '\0')
+while (s[i] != '\0')
 i++;
 return (i);
 }
