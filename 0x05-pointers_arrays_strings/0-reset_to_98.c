72873bb74b4fd2c4932c9290d1286594d625f3e1
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-06-28 23:57:49 -0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
 #include "holberton.h"
-/*
-* reset_to_98 - changes the number
-*@n: the number to change
-*
-*
-*
-*
-*
-*/
+
+/**
+ *reset_to_98 - resets to 98
+ *@n : number
+ *
+ *Return: 0
+ */
 void reset_to_98(int *n)
 {
 *n = 98;
