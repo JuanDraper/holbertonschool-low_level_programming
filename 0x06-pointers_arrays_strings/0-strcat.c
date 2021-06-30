d549cbde4a3bb03d190df8a5561081ca2a5afc3c
JuanDraper <e146613@gmail.com>
2021-06-30 11:09:15 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* _strcat - concatenates two strings
+*@dest: a string
+*@src: another string
+* Return: the concatenated string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+char united[] = (*dest + *src);
+return (dest);
+}
