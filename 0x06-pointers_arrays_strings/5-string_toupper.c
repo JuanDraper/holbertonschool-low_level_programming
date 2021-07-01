3044c37cc5de41859dac975b7470ae392f3baae6
juandraper <e146613@gmail.com>
2021-07-01 11:07:28 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ *
+ */
+
+char *string_toupper(char *)
+{
+int i = 0;
+while (src[i] != '\0')
+{
+	i++;
+	if (src[i] >= 97 && src[i] <= 123)
+	{
+		src[i] -=32;
+	}
+	return src;
+}
