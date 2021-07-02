fb2e100ee5ae95366e65c196ed224c8d1fc88bdd
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-07-01 22:44:20 -0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,18 +8,17 @@
  * @n: Number of bytes to copy
  * Return: @n bytes of @src
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i = 0;
 
-	int i;
-
-	i = 0;
-	while (*(src + i) != 0 && i < n)
+	while (i < n && *(dest + i) != '\0')
 	{
-		*(dest + i) = *(src + i);
+		if (*src != '\0')
+			*(dest + i) = *src++;
+		else
+			*(dest + i) = '\0';
 		i++;
 	}
-	*(dest + i) = *(src + i);
 	return (dest);
 }
