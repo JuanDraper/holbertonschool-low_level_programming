97296cec055bd4d630531f0c42985472b6805ea2
juandraper <e146613@gmail.com>
2021-07-05 10:53:29 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+int i = 0;
+int j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
+}
