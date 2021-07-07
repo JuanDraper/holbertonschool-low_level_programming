0a363f2390fe0d941386bb547c5f328c18787e19
juandraper <e146613@gmail.com>
2021-07-07 09:44:45 -0700

    4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		x =x * _pow_recursion(x, (y - 1));
+	}
+	return (x);
+}
+
