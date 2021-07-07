6ad289a028ef2be319f89fe62df665bdb91f6383
juandraper <e146613@gmail.com>
2021-07-07 10:53:10 -0700

    s

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+int find_square(int n, int i)
+{
+if (i * i == n)
+       {
+       return (i);
+       }
+       if ( i * i > n)
+       {
+       return (-1);
+       }
+       return (n, i + 1);
+       }
+
+/**
+*
+*
+*
+*/
+ int _sqrt_recursion(int n)
+ {
+ return (find_square(n, 1));
+		 }
+
+
