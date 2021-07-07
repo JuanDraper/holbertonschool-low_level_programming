f9e810c6e4d7c6f8d0d3f711e4473ecdebe5c9ba
juandraper <e146613@gmail.com>
2021-07-07 08:55:27 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+	if (*s == '\0')
+	{
+		return(0);
+	}
+	else
+	{
+		i = 1 + _strlen_recursion(s + 1);
+	}
+	return (i);
+}
