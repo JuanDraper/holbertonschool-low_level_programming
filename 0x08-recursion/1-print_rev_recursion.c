5c0f8142b3e16e8f940f44cf1ed12bac6201cd56
juandraper <e146613@gmail.com>
2021-07-07 07:25:13 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,14 +4,15 @@
  *_puts_recursion - print a string
  *@s: is a string
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		_putchar('\n');
-	else
-	{
+		return;
+	
+
 	_puts_recursion(s + 1);
         	_putchar(*s);
 	
-	}
+	
 }
