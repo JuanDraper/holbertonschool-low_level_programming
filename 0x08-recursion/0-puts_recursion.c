d26676c0e2b5062aa0223bfaadb85699044deb1d
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-07-07 11:13:04 -0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
- /**
-  *
-  *
-  *
-  *
-  */
+ 
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 
 void _puts_recursion(char *s)
 {
