83b367505fbe272dd7651fa653d1cc51ca506bae
juandraper <e146613@gmail.com>
2021-07-07 11:13:47 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,10 +10,12 @@
 
 int is_prime_number(int n)
 {  if (n < 0)
+	{
 	n = -n;
-	if (n == 1)
+	}
+	else if (n == 1)
 		return (1);
-	if ( n % 2 == 0 )
+	else if ( n % 2 == 0 )
 		return (0);
 	else
 		return (1);
