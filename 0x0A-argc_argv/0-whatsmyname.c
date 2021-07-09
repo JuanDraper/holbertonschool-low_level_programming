82c053606f50a01363498d81d8e5fcaadb7e3785
juandraper <e146613@gmail.com>
2021-07-09 15:36:59 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print name file new line
+ * @argc: argument
+ * @argv: arguments main and array of strings
+ * Return: 0
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf(%c, argv[0]);
+	return (0);
+}
+
