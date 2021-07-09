9df15bd484e3dcac462545e8df51cf658db3db07
juandraper <e146613@gmail.com>
2021-07-09 16:05:33 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - main 
+ *@argc: blabla
+ *@argv: blabla
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int i;
+
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+			;
+		printf("%d\n", i - 1);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
