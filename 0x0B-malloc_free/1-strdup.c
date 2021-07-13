585013e2d8c937398c73ff1f9f4d6ecf43427079
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-07-13 19:59:46 -0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,32 +1,27 @@
-#include "holberton.h"
 #include <stdlib.h>
+#include "holberton.h"
+
 /**
- * _strdup - check the code for Holberton School students.
- * @str: argument for array
- * Return: Always 0.
+ *_strdup - a
+ *@str : a
+ *
+ *Return: la
  */
 char *_strdup(char *str)
 {
-	char *my_array;
-	int i, len;
+	char *p = NULL;
+	unsigned int i, a;
 
-	my_array = malloc(sizeof(str));
-
-	i = len = 0;
-	while (str[i] != '\0')
+	if (str != NULL)
 	{
-		len++;
-		i++;
+		for (a = 0; str[a]; a++)
+			continue;
+		p = malloc(a + 1);
+		if (p != NULL)
+		{
+			for (i = 0; i < a; i++)
+				p[i] = str[i];
+		}
 	}
-
-	if (my_array == NULL)
-		return (NULL);
-	i = 0;
-	while (str[i] != '\0')
-	{
-		my_array[i] = str[i];
-		i++;
-	}
-
-	return (my_array);
+	return (p);
 }
