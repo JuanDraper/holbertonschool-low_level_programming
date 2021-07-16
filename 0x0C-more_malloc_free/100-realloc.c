3b7fe2bf794be21509ad71bc9f471360fc166a3b
juandraper <e146613@gmail.com>
2021-07-15 19:07:39 -0700

    100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - blabla
+ * @ptr: blabla
+ * @old_size: blabla
+ * @new_size: labla
+ * Return: blabla.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+_putchar('c');
+}
