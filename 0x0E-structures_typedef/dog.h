92104492715cd2fffb741e0be9f072fcd1ddbe5a
juandraper <e146613@gmail.com>
2021-07-19 11:12:03 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog -blalblalbla
+ * @name: blablabla
+ * @owner: blablabla
+ * @age: blablabla
+ */
+typedef struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+} dog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
