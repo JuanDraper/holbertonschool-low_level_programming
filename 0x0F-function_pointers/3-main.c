c217d8bd717f1a7f66a770e83d2854b0d829517c
juandraper <e146613@gmail.com>
2021-07-21 11:01:22 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,27 +12,31 @@
  */
 int main(int argc, char *argv[])
 {
-	int (*f)(int, int);
+	int a, b;
+	int (*o)(int, int);
 
 	if (argc != 4)
 	{
-		puts("Error");
+		printf("Error\n");
 		exit(98);
 	}
-        if (argv[2] && argv[1] != '\0')
+	if (argv[2][1] != '\0')
 	{
-		puts("Error")
-			exit(99);
-	}
-	f = get_op_func(argv[2]);
-
-	if (!f)
-	{
-		puts("Error");
+		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
+	o = get_op_func(argv[2]);
+	if (o == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", o(a, b));
 
 	return (0);
 }
