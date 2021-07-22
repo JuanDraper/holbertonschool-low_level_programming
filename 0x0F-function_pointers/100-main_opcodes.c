f555d8c8992818b5c109a5566c191835e10abc74
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-07-22 10:27:45 -0300

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -23,11 +23,15 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(2);
 	}
-<<<<<<< HEAD
-}
+int j = atoi(argv[1]);
 
-=======
-	for (i = 0; i < atoi(argv[1]) - 1; i++)
-                    ;
-}
->>>>>>> 60ccb841153f9388fd0705c4ecb7f2110804e525
+	for (i = 0; i < j - 1; i++)
+	{
+         printf("%02hhx", *((unsigned char *) main + i));
+		if (i < j - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);           
+	}
