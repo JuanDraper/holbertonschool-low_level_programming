60b1ae8f5e38e51b92ad2f18242d214b1a77e0fe
juandraper <e146613@gmail.com>
2021-07-22 14:41:03 -0700

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,13 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+typedef struct formatTypes
+{
+	char *type;
+	void (*f)(char *separator, va_list vl);
+	/**
+	 *@type: balbla
+	 *Description: blabla
+	 */
+} formatType;
 #endif
