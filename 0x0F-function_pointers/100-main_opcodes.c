8d0a96667c302019f93e3ae7c0409683704578af
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-07-21 21:09:30 -0300

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,13 +1,13 @@
-/**
+
 #include <stdio.h>
 #include <stdlib.h>
 
-
-  * main - print the name of this function
-  * @argc: has the length of the arguments
-  * @argv: has the arguments
-  * Return: 0 to indicate a good working of the program
-  
+/**
+  * main - blabla
+  * @argc: blabla
+  * @argv: blabla
+  * Return: blabla
+*/  
 
 int main(int argc, char **argv)
 {
@@ -24,21 +24,5 @@ int main(int argc, char **argv)
 		exit(2);
 	}
 	for (i = 0; i < atoi(argv[1]) - 1; i++)
-*/
-#include "function_pointers.h"
-
-/**
- * array_iterator - blbalbal
- *@array: blabla
- *@size: bla
- *@action: blabla
- */
-void array_iterator(int *array, size_t size, void (*action)(int))
-{
-size_t i;
-if (array && action)
-for (i = 0; i < size; i++)
-{
-	action(array[i]);
-}
-}
+                    ;
+]
