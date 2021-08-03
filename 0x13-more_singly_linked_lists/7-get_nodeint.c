31a7b612a21e23108b3720da12136ffe9e56080e
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-08-03 18:35:05 -0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
+ * get_nodeint_at_index -  blblalblalb
+ * @index: blalbal
  *
- *
- *
+ * Return: blabla.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
