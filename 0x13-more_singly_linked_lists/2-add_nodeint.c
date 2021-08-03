4b23eabca316ada4cf59c7a0dea7a75d287b0125
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-08-03 18:20:13 -0300

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,23 +1,19 @@
 #include "lists.h"
 
 /**
- * add_nodeint - blabla.
- * @head: blabl.
- * @n: blabal
- * Return: blabbla
+ * add_nodeint -blabla
+ * @head:balbla.
+ * @n: blabla
+ * Return: the address of the new element, or NULL if it failed.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new;
+	listint_t *new_head = (listint_t *) malloc(sizeof(listint_t));
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (new_head == NULL)
 		return (NULL);
-
-	new->n = n;
-	new->next = *head;
-
-	*head = new;
-
-	return (new);
+	new_head->n = n;
+	new_head->next = (*head);
+	(*head) = new_head;
+	return (new_head);
 }
