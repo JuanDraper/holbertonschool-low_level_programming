549e2ba524675150fe70e4354ebc779200c76148
juandraper <e146613@gmail.com>
2021-08-05 16:05:42 -0700

    100-get-endianness.c

---
+++ 0x14-bit_manipulation/100-get-endianness.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *get_endianness - blabla
+ *Return: blbalblabl
+ *
+ *
+ */
+
+int get_endianness(void)
+{
+	unsigned int i;
+	char *c = (char *) &i;
+
+	if (c[0] == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
