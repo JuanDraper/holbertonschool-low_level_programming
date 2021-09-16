296efbe2c0f000fa5fc91e0f30814bb94f382082
juandraper <e146613@gmail.com>
2021-09-16 09:23:43 -0700

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,16 +8,14 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	if (!head)
-	{
 		return (0);
-	}
+
 	while (index)
 	{
 		head = head->next;
 		if (!head)
-		{
 			return (0);
-		}
+		index--;
 	}
 	return (head);
 }
