4aede43485faaafba2cae6a7fb623fab108cdaa9
juandraper <e146613@gmail.com>
2021-10-08 07:12:40 -0700

    fixing

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,24 +1,24 @@
 #include "hash_tables.h"
 /**
-* hash_table_get gets the value of the key-.
+* hash_table_get - gets the value of the key-.
 * @ht: Hash table.
 * @key: Key to search.
 * Return: Value or NULL if not found.
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-        unsigned long int index;
-        hash_node_t *iterator;
+	unsigned long int index;
+	hash_node_t *iterator;
 
-        if (!ht || !key || !strcmp(key, ""))
-                return (NULL);
-        index = key_index((unsigned const char *)key, ht->size);
-        iterator = ht->array[index];
-        while (iterator)
-        {
-                if (!strcmp(iterator->key, key))
-                        return (iterator->value);
-                iterator = iterator->next;
-        }
-        return (NULL);
+	if (!ht || !key || !strcmp(key, ""))
+		return (NULL);
+	index = key_index((unsigned const char *)key, ht->size);
+	iterator = ht->array[index];
+	while (iterator)
+	{
+	if (!strcmp(iterator->key, key))
+		return (iterator->value);
+	iterator = iterator->next;
+	}
+	return (NULL);
 }
